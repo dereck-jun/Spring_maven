a71e40eea9737f6ff398320d2a9a51056d39424e
dereck <yjpark0805@naver.com>
2023-10-06 16:42:19 +0900

    인터페이스를 활용하도록 만들기

--- ch02_calculator/src/main/java/com/calculator/ch02/MainClass.java
+++ ch02_calculator/src/main/java/com/calculator/ch02/MainClass.java
@@ -4,9 +4,9 @@ public class MainClass {
   public static void main(String[] args) {
     MyCalculator calculator = new MyCalculator();
     // CalAdd 를 외부에서 주입 -> MyCalculator 는 연산에 필요한 객체를 직접 생성하지 않고 외부에서 받아서 사용
-    calculator.calAdd(10, 5, new CalAdd());
-    calculator.calSub(10, 5, new CalSub());
-    calculator.calMul(10, 5, new CalMul());
-    calculator.calDiv(10, 5, new CalDiv());
+    calculator.calculate(10, 5, new CalAdd());
+    calculator.calculate(10, 5, new CalSub());
+    calculator.calculate(10, 5, new CalMul());
+    calculator.calculate(10, 5, new CalDiv());
   }
 }
--- ch02_calculator/src/main/java/com/calculator/ch02/MyCalculator.java
+++ ch02_calculator/src/main/java/com/calculator/ch02/MyCalculator.java
@@ -1,23 +1,8 @@
 package com.calculator.ch02;
 
 public class MyCalculator {
-  public void calAdd(int num1, int num2, CalAdd add) { // 객체 주입 (DI)
-    int value = add.doOperation(num1, num2);
-    System.out.println("result: " + value);
-  }
-
-  public void calSub(int num1, int num2, CalSub sub) {
-    int value = sub.doOperation(num1, num2);
-    System.out.println("result: " + value);
-  }
-
-  public void calMul(int num1, int num2, CalMul mul) {
-    int value = mul.doOperation(num1, num2);
-    System.out.println("result: " + value);
-  }
-
-  public void calDiv(int num1, int num2, CalDiv div) {
-    int value = div.doOperation(num1, num2);
+  public void calculate(int num1, int num2, ICalculator iCalculator) {
+    int value = iCalculator.doOperation(num1, num2);
     System.out.println("result: " + value);
   }
 }
